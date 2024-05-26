4a6300f2dde6b59bc21734372d0f3a97dd2ed8d8
GiwrgosP <gparaskevopoulos@amcstudent.edu.gr>
2024-05-26 23:12:15 +0300

    buttonList - baloon
    δημιούργησα ένα νέο obj για το buttonList στο epiloges.
    πρόσθεσα και τις αλλαγές σου για το baloon στο epiloges

---
+++ buttonList.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+import pyperclip
+import scrollableFrame as scrollableFrame
+import dbOrNotdb as dbUtility
+
+class buttonList(tk.Tk):
+
+    def __del__(self):
+        print(self.delMsg)
+
+    def __init__(self,master,query,delMsg):
+        self.master = master
+        self.query = query
+        self.delMsg = delMsg
+        self.slaveList = None
+        self.but = {}
+        self.valueSelected = None
+
+
+        self.frame = tk.Frame(self.master.master.mainFrame,bg = "#b9b5b5", borderwidth = 5, relief = "groove")
+        self.frame.pack(side = "left", fill = "y", anchor = "nw")
+
+    def createButtonList(self, id):
+
+        self.scrolledFrame = scrollableFrame.VerticalScrolledFrame(self.frame)
+        self.scrolledFrame.configure(borderwidth = 5, relief = "groove")
+        self.scrolledFrame.pack(fill = "y", expand =True)
+
+        butList = dbUtility.get(self.master.master.path,(self.query,(id,)))
+
+        for widget in butList:
+            id,title = widget
+            but = tk.Button(self.scrolledFrame.interior, width = 20, wraplength = 100,  justify = "left", text = title, command = lambda x = id, y = title : self.butFunction(x,y), relief = "groove")
+            but.pack(side = "top", fill = "x")
+            self.but[id] = but
+
+    def butFunction(self,id,title):
+        if self.valueSelected != None:
+            self.but[self.valueSelected].configure(state = "normal")
+
+        self.but[id].configure(state = "disabled")
+
+        self.valueSelected = id
+
+        self.slaveList.deleteButtonList()
+        self.slaveList.createButtonList(id)
+
+    def deleteButtonList(self):
+        for item in self.frame.winfo_children():
+            item.destroy()
+        self.slaveList.deleteButtonList()
+        self.but.clear()
+        self.valueSelected = None
